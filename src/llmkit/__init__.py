716703f254add7d16eed563af3c981b16bd6d2f4
PaulAduGyamfi <pjadu7@gmail.com>
2026-09-22 13:29:39 -0400

    add re-export to init

---
+++ src/llmkit/__init__.py
@@ -0,0 +1,5 @@
+from llmkit.client import LLMClient
+from llmkit.logging import configure, request_id
+from llmkit.settings import Settings, get_settings
+
+__all__ = ["LLMClient", "Settings", "configure", "get_settings", "request_id"]
